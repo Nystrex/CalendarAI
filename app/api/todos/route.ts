dc64d3e2ed2abfaf69e90c14976b1b7645b67496
Mohammed Al Cacouni <alcacounihadi@gmail.com>
2026-02-25 01:48:23 -0500

    test

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
     if (!user) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
     }
-
+    //Hi
     const { data: todos, error } = await supabase
       .from("todos")
       .select("*")
